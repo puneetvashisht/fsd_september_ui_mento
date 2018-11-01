0b0cff93b93f4da283443c0d8cc886640c648248
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2018-11-01 14:52:55 +0530

    fix deletion

--- my-first-app/src/app/components/workout.component.ts
+++ my-first-app/src/app/components/workout.component.ts
@@ -6,7 +6,7 @@ import { Http } from '@angular/http';
     template: `
      <h2>Display workouts</h2>
      <ul>
-        <li *ngFor="let workout of workouts"> {{workout.workoutTitle}} <button (click)="deleteWorkout(workout._id)">Delete</button></li>
+        <li *ngFor="let workout of workouts; let i=index"> {{workout.workoutTitle}} <button (click)="deleteWorkout(workout._id,i)">Delete</button></li>
      </ul>
     `
 })
@@ -28,16 +28,18 @@ export class WorkoutComponent implements OnInit {
     }
 
 
-    deleteWorkout(workoutid){
+    deleteWorkout(workoutid, index){
         console.log('Delete workout', workoutid)
-
+        console.log(index)
         this.http.delete('http://localhost:3000/workouts/' + workoutid)
         .toPromise()
         .then(res=>{
             console.log(res.status)
             return res.json()
         })
-        .then(data=> console.log(data))
+        .then(data=>{this.workouts.splice(index, 1)
+            console.log(data)
+        } )
     }
 
 }
